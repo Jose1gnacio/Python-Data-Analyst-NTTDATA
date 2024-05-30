9113d06c7a3b1f726d6ba99682dd72c8336b2bed
Jose1gnacio <joseignacio.alvarez.n@gmail.com>
2024-05-29 23:49:12 -0400

    fin py mongodb

--- Area_2/Modulo_2_Ejercicios_Py_MongoDB.py
+++ Area_2/Modulo_2_Ejercicios_Py_MongoDB.py
@@ -40,6 +40,9 @@ except pymongo.errors.ServerSelectionTimeoutError as errorTiempo:
 except pymongo.errors.ConnectionFailure as errorConexion:
     print("Fallo al conectarse a mongodb "+errorConexion) """
 
+from tkinter import*
+from tkinter import ttk
+from tkinter import messagebox
 import pymongo
 
 MONGO_HOST="localhost"
@@ -120,31 +123,134 @@ except pymongo.errors.ConnectionFailure as errorConexion:
     print("Fallo al conectarse a mongodb "+errorConexion) """
 
 #2.4.4
-MONGO_BASEDATOS="sample_training"
-MONGO_COLECCION="companies"
+""" MONGO_BASEDATOS="sample_training"
+MONGO_COLECCION="inspections"
+
+cliente=pymongo.MongoClient(MONGO_URL_CLOUD,serverSelectionTimeoutMS=MONGO_TIME_OUT)
+cliente.server_info()
+
+baseDatos=cliente[MONGO_BASEDATOS]
+coleccion=baseDatos[MONGO_COLECCION]
+
+def mostrarDatos():
+    try:
+        registros=tabla.get_children()
+        for registro in registros:
+            tabla.delete(registro)
+        for documento in coleccion.find():
+            tabla.insert('',0,text=documento["business_name"],values=documento["certificate_number"])
+
+        cliente.close()   
+    except pymongo.errors.ServerSelectionTimeoutError as errorTiempo:
+        print("Tiempo exedido "+errorTiempo)
+    except pymongo.errors.ConnectionFailure as errorConexion:
+        print("Fallo al conectarse a mongodb "+errorConexion)
+    
+ 
+ventana=Tk()
+tabla=ttk.Treeview(ventana,columns=2)
+tabla.grid(row=1,column=0,columnspan=2)
+tabla.heading("#0",text="Nombre de la empresa")
+tabla.heading("#1",text="Número de certificado")
+
+mostrarDatos()
+ 
+#Mostramos la ventana
+ventana.mainloop() """
+
+#2.4.5
+""" MONGO_BASEDATOS="sample_training"
+MONGO_COLECCION="zips"
+
 try:
     cliente=pymongo.MongoClient(MONGO_URL_CLOUD,serverSelectionTimeoutMS=MONGO_TIME_OUT)
     cliente.server_info()
-    #print("Conexion con mongo exitosa")
 
     baseDatos=cliente[MONGO_BASEDATOS]
     coleccion=baseDatos[MONGO_COLECCION]
 
-    filter = {"number_of_employees": {"$gt": 250}}
-    for documento in coleccion.find(filter, {"name": 1, "number_of_employees": 1}):
-        nombre = documento.get("name")
-        numero_empleados = documento.get("number_of_employees")
-        print(f"Compañía: {nombre} - Número de empleados: {numero_empleados}")    
-        
-    cliente.close()
+    nuevo_documento = {
+        "city": "Madrid",
+        "zip": "458971",
+        "loc": [40.342553, -3.171997],
+        "pop": 11111,
+        "state": "SP"
+    }
+
+    resultado = coleccion.insert_one(nuevo_documento)
+    if resultado.inserted_id:
+        print("El nuevo documento se insertó correctamente:")
+        print(nuevo_documento)
+    else:
+        print("Hubo un error al insertar el documento.")
+
+
+    cliente.close()   
 except pymongo.errors.ServerSelectionTimeoutError as errorTiempo:
-    print("Tiempo excedido "+errorTiempo)
+    print("Tiempo exedido "+errorTiempo)
 except pymongo.errors.ConnectionFailure as errorConexion:
     print("Fallo al conectarse a mongodb "+errorConexion)
+     """
 
+#2.4.6
+""" MONGO_BASEDATOS="sample_training"
+MONGO_COLECCION="zips"
 
+try:
+    cliente=pymongo.MongoClient(MONGO_URL_CLOUD,serverSelectionTimeoutMS=MONGO_TIME_OUT)
+    cliente.server_info()
 
+    baseDatos=cliente[MONGO_BASEDATOS]
+    coleccion=baseDatos[MONGO_COLECCION]
 
+    filter = {"city": "Madrid"}
+    documentos = coleccion.find(filter)
 
+    for documento in documentos:
+        print(documento)
+   
 
+    cliente.close()   
+except pymongo.errors.ServerSelectionTimeoutError as errorTiempo:
+    print("Tiempo exedido "+errorTiempo)
+except pymongo.errors.ConnectionFailure as errorConexion:
+    print("Fallo al conectarse a mongodb "+errorConexion) """
+    
+#2.4.7
+MONGO_BASEDATOS="sample_training"
+MONGO_COLECCION="zips"
 
+cliente=pymongo.MongoClient(MONGO_URL_CLOUD,serverSelectionTimeoutMS=MONGO_TIME_OUT)
+cliente.server_info()
+
+baseDatos=cliente[MONGO_BASEDATOS]
+coleccion=baseDatos[MONGO_COLECCION]
+
+def mostrarDatos():
+    try:
+        registros=tabla.get_children()
+        for registro in registros:
+            tabla.delete(registro)
+        for documento in coleccion.find({"city": "BREMEN"}):
+            zip_code = documento.get("zip", "Desconocido")
+            pop = documento.get("pop", "Desconocido")
+            tabla.insert('', 0, text=zip_code, values=(pop,))
+            
+
+        cliente.close()   
+    except pymongo.errors.ServerSelectionTimeoutError as errorTiempo:
+        print("Tiempo exedido "+errorTiempo)
+    except pymongo.errors.ConnectionFailure as errorConexion:
+        print("Fallo al conectarse a mongodb "+errorConexion)
+    
+ 
+ventana=Tk()
+ventana.title("Zips and population of Bremen")
+tabla=ttk.Treeview(ventana,columns=2)
+tabla.grid(row=1,column=0,columnspan=2)
+tabla.heading("#0",text="Zips")
+tabla.heading("#1",text="Population")
+
+mostrarDatos()
+
+ventana.mainloop()
