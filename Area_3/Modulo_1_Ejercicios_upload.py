9e6f8890b281646aeb0aa163cbdceab7ac791a21
Jose1gnacio <joseignacio.alvarez.n@gmail.com>
2024-06-05 22:40:16 -0400

    Area 3

---
+++ Area_3/Modulo_1_Ejercicios_upload.py
@@ -0,0 +1,48 @@
+#3.1.1
+""" import csv
+
+ruta_archivo = 'C:/Users/josei/Desktop/apellidos_mas_frecuentes_pais_Argentina.csv'
+
+with open(ruta_archivo, newline='', encoding='utf-8') as csv_file:
+    lector_csv = csv.reader(csv_file)  
+    
+    for fila in lector_csv:
+        print(fila) """
+
+
+
+#3.1.2
+""" import csv
+
+ruta_archivo = 'C:/Users/josei/Desktop/apellidos_mas_frecuentes_pais_Argentina.csv'
+
+with open(ruta_archivo, newline='', encoding='utf-8') as csv_file:
+    lector_csv = csv.reader(csv_file) 
+
+    next(lector_csv)  
+    
+    for fila in lector_csv:
+        if float(fila[1]) > 0.1:
+            print(fila[0]) """
+
+
+#3.1.3
+import numpy as np
+
+ruta_archivo = 'C:/Users/josei/Desktop/apellidos_mas_frecuentes_pais_Argentina.csv'
+
+data = np.genfromtxt(ruta_archivo, delimiter=',', dtype=None, encoding='utf-8', names=True)
+
+apellidos = data['apellido']
+porcentajes = data['porcentaje_de_poblacion_portadora']
+rankings = data['ranking']
+
+print("Apellidos:")
+print(apellidos)
+print("\nPorcentajes de población portadora:")
+print(porcentajes)
+print("\nRankings:")
+print(rankings)
+
+
+        
