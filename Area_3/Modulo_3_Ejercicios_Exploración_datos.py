fc45e69e4784a2f491c54d76f304fa55d911bb39
Jose1gnacio <joseignacio.alvarez.n@gmail.com>
2024-06-17 15:43:00 -0400

    Ejer modulo 3

--- Area_3/Modulo_3_Ejercicios_Exploración_datos.py
+++ Area_3/Modulo_3_Ejercicios_Exploración_datos.py
@@ -1,10 +1,16 @@
 import pandas as pd
 
-#3.4.1
 archivo_csv = r'C:\Users\josei\Documents\NTT DATA CLASES\Área 3 - Material de clases\Modulo_3\Esculturas_Parque_Arqueolgicos_Colombia.csv'
 df = pd.read_csv(archivo_csv)
-registro_105 = df.iloc[105]
+
+# 3.4.1 (using double backslashes)
+""" registro_105 = df.iloc[105]
 print(registro_105)
 
 nombre_elemento = registro_105['Nombre elemento arqueológico']
-print(f'Nombre del elemento buscado es: {nombre_elemento}')
+print(f'Nombre del elemento buscado es: {nombre_elemento}') """
+
+# 3.4.2
+registro = df.loc[[24, 36, 125, 182], ['Nombre elemento arqueológico', 'Ubicación actual dentro del parque']]
+print(registro)
+
