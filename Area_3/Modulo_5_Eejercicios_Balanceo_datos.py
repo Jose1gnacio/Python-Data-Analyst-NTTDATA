77f0e0c70599ce0b32ce1781d535b9b721819f78
Jose1gnacio <joseignacio.alvarez.n@gmail.com>
2024-07-04 22:45:33 -0400

    Eje balanceo2

--- Area_3/Modulo_5_Eejercicios_Balanceo_datos.py
+++ Area_3/Modulo_5_Eejercicios_Balanceo_datos.py
@@ -37,7 +37,7 @@ pred_y = model.predict(X_test)
 mostrar_resultados(y_test, pred_y) """
 
 #3.10.2
-from sklearn.datasets import load_iris
+""" from sklearn.datasets import load_iris
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -78,6 +78,54 @@ print("Distribución antes del balanceo {}".format(Counter(y_train)))
 print("Distribución después del balanceo {}".format(Counter(y_train_res)))
 
 #Entrenamos y testeamos
+model = run_model(X_train_res, X_test, y_train_res, y_test)
+pred_y = model.predict(X_test)
+mostrar_resultados(y_test, pred_y) """
+
+#3.10.3
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+import seaborn as sns
+from imblearn.over_sampling import RandomOverSampler
+from collections import Counter
+
+# Cargar el dataset iris
+iris = load_iris()
+X, y = iris.data, iris.target
+
+# Dividir los datos en conjuntos de entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=1)
+
+# Aplicar RandomOverSampling para balancear los datos
+ros = RandomOverSampler(random_state=1)
+X_train_res, y_train_res = ros.fit_resample(X_train, y_train)
+
+# Definir la función que vamos a necesitar para entrenar el modelo
+def run_model(X_train, X_test, y_train, y_test):
+    clf = LogisticRegression(C=1.0, penalty='l2', random_state=1, solver="newton-cg")
+    clf.fit(X_train, y_train)
+    return clf
+
+# Mostrar los resultados
+def mostrar_resultados(y_test, pred_y):
+    conf_matrix = confusion_matrix(y_test, pred_y)
+    plt.figure(figsize=(8, 8))
+    sns.heatmap(conf_matrix, xticklabels=iris.target_names, yticklabels=iris.target_names, annot=True, fmt="d", cmap='Blues')
+    plt.title("Confusion Matrix")
+    plt.ylabel('True Class')
+    plt.xlabel('Predicted Class')
+    plt.show()
+    
+    print(classification_report(y_test, pred_y))
+
+# Mostrar los valores antes y después de balancear los datos
+print("Distribución antes del balanceo {}".format(Counter(y_train)))
+print("Distribución después del balanceo {}".format(Counter(y_train_res)))
+
+# Entrenar y testear el modelo
 model = run_model(X_train_res, X_test, y_train_res, y_test)
 pred_y = model.predict(X_test)
 mostrar_resultados(y_test, pred_y)
